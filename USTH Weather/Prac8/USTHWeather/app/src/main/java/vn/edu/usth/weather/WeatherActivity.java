952e8f48e8380deda4693b5f41ee09ad029926e9
nhphat29 <nhphat04@gmail.com>
2024-09-30 21:38:41 +0700

    Practical 12

---
+++ USTH Weather/Prac8/USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,109 @@
+package vn.edu.usth.weather;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String TAG = "Weather";
+
+    private String[] cities = {"Hanoi", "Paris", "Toulouse"};
+    private int[] weatherIcons = {R.drawable.cloudy, R.drawable.partly_cloudy, R.drawable.cloudy};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather);
+        Log.i(TAG, "Create");
+
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        WeatherPagerAdapter adapter = new WeatherPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Hanoi");
+                    break;
+                case 1:
+                    tab.setText("Paris");
+                    break;
+                case 2:
+                    tab.setText("Toulouse");
+                    break;
+            }
+        }).attach();
+
+        MediaPlayer music = MediaPlayer.create(WeatherActivity.this, R.raw.music);
+        music.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_setting) {
+            Intent intent = new Intent(this, PrefActivity.class);
+            this.startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.i(TAG, "Start");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.i(TAG, "Resume");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.i(TAG, "Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Stop");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(TAG, "Destroy");
+    }
+}
